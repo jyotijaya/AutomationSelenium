83afd7b6d76adf588828aa8eec8cc77ae3304ec3
jyotijaya <prakash.mishra201717@gmail.com>
2024-01-29 01:40:30 +0530

    Adding print statement

--- com.intra/src/test/java/com/intra/Demo.java
+++ com.intra/src/test/java/com/intra/Demo.java
@@ -7,11 +7,13 @@ import org.testng.annotations.Test;
 public class Demo {
 
 	@Test
-	public void m1()
+	public void TC_01()
 	{
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Babul\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		System.out.println("Launching Browser");
 		WebDriver driver=new ChromeDriver();
+		System.out.println("Quit browser");
 		driver.quit();
 	}
 	
